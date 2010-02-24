5d79ce57b64d9881351f7b6a8ee5bec52755be3f
Snoyman <snoyman@snoytop-karmic.(none)>
2010-02-24 18:30:11 +0200

    Fixed toLBS and toSource via Stream

--- Network/Wai/Enumerator.hs
+++ Network/Wai/Enumerator.hs
@@ -17,15 +17,13 @@ module Network.Wai.Enumerator
     , fromEitherFile
     ) where
 
-import Network.Wai (Enumerator (..))
+import Network.Wai (Enumerator (..), Iteratee)
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString as B
 import System.IO.Unsafe (unsafeInterleaveIO)
 import System.IO (withBinaryFile, IOMode (ReadMode), Handle, hIsEOF)
 import Data.ByteString.Lazy.Internal (defaultChunkSize)
-import Control.Concurrent (forkIO)
 import Control.Concurrent.MVar
-import Data.Function (fix)
 
 -- | Performs a specified conversion on each 'B.ByteString' output by an
 -- enumerator.
@@ -36,17 +34,13 @@ mapE f (Enumerator e) = Enumerator $ \rec iter -> e rec (iter' iter) where
 -- | This uses 'unsafeInterleaveIO' to lazily read from an enumerator. All
 -- normal lazy I/O warnings apply.
 toLBS :: Enumerator -> IO L.ByteString
-toLBS e = do
-    source <- toSource e
-    L.fromChunks `fmap` helper source
-      where
-        helper source = unsafeInterleaveIO $ do
-                x <- source
-                case x of
-                    Nothing -> return []
-                    Just x' -> do
-                        xs <- helper source
-                        return $ x' : xs
+toLBS e = fmap L.fromChunks (toStream e >>= go) where
+    go :: Stream -> IO [B.ByteString]
+    go Nil = return []
+    go (Cons bs rest) = do
+        rest' <- unsafeInterleaveIO $ rest >>= go
+        return $ bs : rest'
+    go _ = error "Invalid Stream in toLBS"
 
 -- | This function safely converts a lazy bytestring into an enumerator.
 fromLBS :: L.ByteString -> IO Enumerator
@@ -77,28 +71,31 @@ fromLBS' lbs' = Enumerator $ \rec iter a0 -> lbs' >>= \lbs -> do
 -- Each time you call it, it returns the next chunk of data if available, or
 -- 'Nothing' if the data has been completely consumed.
 toSource :: Enumerator -> IO (IO (Maybe B.ByteString))
-toSource (Enumerator e) = do
-    buffer <- newEmptyMVar
-    _ <- forkIO $ fix e (helper buffer) () >> putMVar buffer Nothing
-    return $ source buffer
+toSource e = do
+    mstream <- toStream e >>= newMVar
+    return $ go mstream
       where
-        helper :: MVar (Maybe B.ByteString)
-               -> ()
-               -> B.ByteString
-               -> IO (Either () ())
-        helper buffer _ bs = do
-            putMVar buffer $ Just bs
-            return $ Right ()
-        source :: MVar (Maybe B.ByteString) -> IO (Maybe B.ByteString)
-        source mmbs = do
-            mbs <- takeMVar mmbs
-            case mbs of
-                Nothing -> do
-                    -- By putting Nothing back in, the source can be called
-                    -- again without causing a deadlock.
-                    putMVar mmbs Nothing
-                    return Nothing
-                Just bs -> return $ Just bs
+        go :: MVar Stream -> IO (Maybe B.ByteString)
+        go mstream = modifyMVar mstream go'
+        go' :: Stream -> IO (Stream, Maybe B.ByteString)
+        go' Nil = return (Nil, Nothing)
+        go' (Cons bs rest) = do
+            rest' <- rest
+            return (rest', Just bs)
+        go' _ = error "Invalid Stream in toSource"
+
+data Stream = Nil
+            | Elem B.ByteString
+            | Cons B.ByteString (IO Stream)
+toStream :: Enumerator -> IO Stream
+toStream (Enumerator enum) = fmap (either id id) $ enum rec iter Nil where
+    iter :: Iteratee Stream
+    iter _ c = return $ Right $ Elem c
+    rec :: Iteratee Stream -> Stream -> IO (Either Stream Stream)
+    rec _ Nil = return $ Right Nil
+    rec iter' (Elem c) = return $ Right $ Cons c
+                      $ fmap (either id id) $ enum rec iter' Nil
+    rec _ _ = error "Invalid Stream to toStream.rec"
 
 -- | Read a chunk of data from the given 'Handle' at a time. We use
 -- 'defaultChunkSize' from the bytestring package to determine the largest
